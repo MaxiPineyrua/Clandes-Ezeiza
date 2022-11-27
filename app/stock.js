971d8b2dd856f96fc6fca9d56b9d27cb6860af57
maxipineyrua <pineyruamaximiliano@gmail.com>
2022-11-27 02:37:49 -0300

    Página actualizada, agregado JavaScript

---
+++ app/stock.js
@@ -0,0 +1,148 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Calco Argentina",
+        tipo: "Calcomanía",
+        desc: "Para que tu moto quede flama.",
+        precio: 200,
+        talle: "",
+        img: '../images/logoclandesarg.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Calco Brasil",
+        tipo: "Calcomanía",
+        desc: "Para que tu moto quede flama.",
+        precio: 200,
+        talle: "",
+        img: "../images/logoclandesbrasil.jpg",
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Calco Instagram 1",
+        tipo: "Calcomanía",
+        desc: "Para que tu moto quede flama.",
+        precio: 200,
+        talle: "",
+        img: '../images/logoclandesinsta.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Calco Instagram 2",
+        tipo: "Calcomanía",
+        desc: "Para que tu moto quede flama.",
+        precio: 200,
+        talle: "",
+        img: '../images/logoclandesinsta2.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Calco Italia",
+        tipo: "Calcomanía",
+        desc: "Para que tu moto quede flama.",
+        precio: 200,
+        talle: "",
+        img: '../images/logoclandesitalia.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Calco Nike",
+        tipo: "Calcomanía",
+        desc: "Para que tu moto quede flama.",
+        precio: 200,
+        talle: "",
+        img: '../images/logoclandesnike.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 7,
+        nombre: "Calco Noba",
+        tipo: "Calcomanía",
+        desc: "Para tener al Noba presente.",
+        precio: 200,
+        talle: "",
+        img: '../images/logoclandesnoba.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 8,
+        nombre: "Calco USA",
+        tipo: "Calcomanía",
+        desc: "Para que tu moto quede flama.",
+        precio: 200,
+        talle: "",
+        img: '../images/logoclandesusa.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 9,
+        nombre: "Chomba Yamaha Azul",
+        tipo: "Ropa",
+        desc: "Una chomba que va con tu moto.",
+        precio: 7500,
+        talle: "L",
+        img: '../images/remeraazulyamaha.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 10,
+        nombre: "Chomba Yamaha Negra",
+        tipo: "Ropa",
+        desc: "Una chomba que va con tu moto.",
+        precio: 7500,
+        talle: "L",
+        img: '../images/remeranegrayamaha.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 11,
+        nombre: "Remera Ferrari Negra",
+        tipo: "Ropa",
+        desc: "Una remera que va con tu moto.",
+        precio: 6000,
+        talle: "L",
+        img: '../images/remeraclandesferrari.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 12,
+        nombre: "Remera Honda Negra",
+        tipo: "Ropa",
+        desc: "Una remera que va con tu moto.",
+        precio: 6000,
+        talle: "L",
+        img: '../images/remeraclandeshonda.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 13,
+        nombre: "Remera Honda Negra Mujer",
+        tipo: "Ropa",
+        desc: "Una remera que va con tu moto.",
+        precio: 6000,
+        talle: "L",
+        img: '../images/remeraclandesmujer.jpg',
+        cantidad:1
+    },
+
+]
+
+export { productos };
